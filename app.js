ad05fb84e34fd7cb3c0aa8cdb45b69e3df057ec7
mremoto <mremotoo@gmail.com>
2022-03-25 11:20:17 +0000

    Add, subtract, multiply, divide functions, with the operator function calling the above functions

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log("Hello World!")
+//add, subtract, multiply, divide functions
+const addNum = (num1, num2) => {console.log( num1 + num2)}
+const subtractNum = (num1, num2) => {console.log( num1 - num2)}
+const multiplyNum = (num1, num2) => {console.log( num1 * num2)}
+const divideNum = (num1, num2) => {console.log( num1 / num2)}
+
+//operator function takes an operator and 2 numbers then calls one of the above functions on the numbers
+const operate = (num1, num2, operator) => {
+    console.log(num1, num2, operator)
+    if (operator == "+") {
+        addNum(num1, num2)
+    }else if (operator == "-") {
+        subtractNum(num1, num2)
+    }else if (operator == "x" || operator == "*") {
+        multiplyNum(num1, num2)
+    }else if(operator == "/") {
+        divideNum(num1, num2)
+    }
+}
